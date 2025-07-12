3404fb4137df0462a39620225f26396150ffee1b
Zenhar <zenhar.mz@gmail.com>
2025-07-12 17:28:50 +0600

    feat: add user endpoints to test decorators

--- apps/backend/src/users/users.module.ts
+++ apps/backend/src/users/users.module.ts
@@ -1,11 +1,13 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './entities/user.entity';
-import { UsersRepository } from './repositories/users.repository';
-import { UsersService } from './services/users.service';
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { User } from "./entities/user.entity";
+import { UsersRepository } from "./repositories/users.repository";
+import { UsersService } from "./services/users.service";
+import { UsersController } from "./controllers/users.controller";
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
+  controllers: [UsersController],
   providers: [UsersRepository, UsersService],
   exports: [UsersService],
 })
